28d88d6f085538035ffd204866f6c4d0799799aa
user <allegrotech89@gmail.com>
2022-01-18 00:26:12 +0100

    Carousel fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,39 +6,47 @@ import slideData from "../slideData";
 import styled from "styled-components";
 
 function Caro() {
-	const Styles = styled.div`
-		color: #fff;
-		font-size: 2rem;
+	const BlackText = styled.div`
+		color: #000;
+		font-size: 1.5rem;
 		font-weight: bold;
-		.w-100 {
-			width: 100%;
-		}
 	`;
+	const WhiteText = styled.div`
+		color: #fff;
+		font-size: 1.5rem;
+		font-weight: bold;
+	`;
+
 	const pics = slideData.products;
 	return (
-		<Container fluid>
+		<Container>
 			<Carousel>
-				<Carousel.Item interval={2000}>
-					<img fluid className="d-block  " src={pics[0].image} alt="First slide" />
+				<Carousel.Item interval={4000}>
+					<img fluid className="d-block w-100" src={pics[0].image} alt="Second slide" />
 					<Carousel.Caption>
-						<Styles>
-							<h3 className="slid-1">Get 30% off This watch</h3>
-							<p>A gold wrist watch is all you need to up your fashion game</p>
-						</Styles>
+						<BlackText>
+							<h3 className="slid-2">Fashion has never benn better with Gtone MultiBizz</h3>
+							<p>Lorem ipsum dolor sit.</p>
+						</BlackText>
 					</Carousel.Caption>
 				</Carousel.Item>
-				<Carousel.Item interval={2000}>
+
+				<Carousel.Item interval={4000}>
 					<img fluid className="d-block w-100" src={pics[1].image} alt="Second slide" />
 					<Carousel.Caption>
-						<h3 className="slid-2">Fashion has never benn better with Gtone MultiBizz</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<BlackText>
+							<h3 className="slid-2">Better fashion with us</h3>
+							<p>Lorem ipsum dolor sit </p>
+						</BlackText>
 					</Carousel.Caption>
 				</Carousel.Item>
-				<Carousel.Item interval={2000}>
+				<Carousel.Item interval={4000}>
 					<img fluid className=" w-100" src={pics[2].image} alt="Third slide" />
 					<Carousel.Caption>
-						<h3 className="slid-3">Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						<WhiteText>
+							<h3 className="slid-3">Don't just be cool</h3>
+							<p>Look cool! with Gtone accessories</p>
+						</WhiteText>
 					</Carousel.Caption>
 				</Carousel.Item>
 			</Carousel>
